be201eedfc857c331b4585f7774fa2d72b30efee
ManishShubhu <lifestartedfrom1997@gmail.com>
2019-04-23 23:13:39 +0545

    login design completed

--- app/src/main/java/com/onlinevotingsystem/MainActivity.java
+++ app/src/main/java/com/onlinevotingsystem/MainActivity.java
@@ -2,12 +2,42 @@ package com.onlinevotingsystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLogin;
+    private TextView tvSignup;
+    private EditText etUsername, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        tvSignup = findViewById(R.id.tvSignup);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(this);
+        tvSignup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( btnLogin.getId() == v.getId() )
+        {
+            //Do Button1 click operations here
+
+        }
+        else if( tvSignup.getId() == v.getId() )
+        {
+
+            // Do Button2 click operations here
+
+        }
     }
 }
